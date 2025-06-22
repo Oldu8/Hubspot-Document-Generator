1c63fe27fc1c2466ea3fb0f1bd5fadd1accdea00
Oldu8 <dudkoleg@gmail.com>
2025-06-22 16:03:50 +0200

    add backend to work with docs file

--- hs_generator_oldu_v1/src/app/app.functions/generate.js
+++ hs_generator_oldu_v1/src/app/app.functions/generate.js
@@ -1,43 +1,59 @@
-const hubspot = require("@hubspot/api-client");
+const fs = require("fs");
+const path = require("path");
+const PizZip = require("pizzip");
+const Docxtemplater = require("docxtemplater");
 
 exports.main = async (context = {}) => {
   try {
-    const { propertiesToSend = {}, parameters = {} } = context;
+    const { parameters = {}, propertiesToSend = {} } = context;
 
-    // Преобразуем данные в текст
-    const lines = [
-      `Document generated for user ID: ${parameters.userId}`,
-      "---",
-      ...Object.entries(propertiesToSend).map(
-        ([key, value]) => `${key}: ${value ?? "null"}`
-      ),
-    ];
-    const content = lines.join("\n");
+    const { doc_name = "" } = parameters;
+    const templatePath = path.join(
+      __dirname,
+      "../templates",
+      `${doc_name}.docx`
+    );
 
-    // Преобразуем в Buffer
-    const buffer = Buffer.from(content, "utf-8");
+    const content = fs.readFileSync(templatePath, "binary");
+    const zip = new PizZip(content);
+    const doc = new Docxtemplater(zip, {
+      paragraphLoop: true,
+      linebreaks: true,
+    });
 
-    // Название файла
-    const filename = `deal_info_${Date.now()}.txt`;
+    // const data = {
+    //   institution_name: propertiesToSend.institution_name_sync,
+    //   gme_id: propertiesToSend.acgme_institution_id_sync,
+    // };
+
+    // doc.setData(data);
+
+    try {
+      doc.render();
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: { error: "Template rendering failed", details: err },
+      };
+    }
+
+    const buf = doc.getZip().generate({ type: "nodebuffer" });
+    const base64Doc = buf.toString("base64");
 
     return {
       statusCode: 200,
       body: {
         success: true,
-        message: "TXT document generated",
-        filename,
-        document: buffer.toString("base64"),
-        mimeType: "text/plain",
+        filename: `${doc_name}_${Date.now()}.docx`,
+        mimeType:
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        document: base64Doc,
       },
     };
-  } catch (error) {
-    console.error("Error generating TXT file:", error);
+  } catch (err) {
     return {
       statusCode: 500,
-      body: {
-        success: false,
-        error: error.message,
-      },
+      body: { error: err.message },
     };
   }
 };
